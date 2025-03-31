35fc9a781433246114e4dd59f887b528421da7ad
Wang Yue <yyuewang@amazon.com>
2025-03-31 21:18:01 +0800

    test

--- src/main/java/com/sdxpub/feishubot/config/CacheConfig.java
+++ src/main/java/com/sdxpub/feishubot/config/CacheConfig.java
@@ -1,6 +1,7 @@
 package com.sdxpub.feishubot.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.Cache;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.Executors;
 
 @Configuration
 @EnableCaching
@@ -29,5 +32,15 @@ public class CacheConfig {
                 .expireAfterWrite(12, TimeUnit.HOURS)
                 .initialCapacity(100)
                 .maximumSize(10000);
+    }
+
+    @Bean
+    public Cache<String, com.sdxpub.feishubot.model.message.Session> sessionCache(Caffeine<Object, Object> caffeineConfig) {
+        return caffeineConfig.build();
+    }
+
+    @Bean
+    public ScheduledExecutorService scheduledExecutorService() {
+        return Executors.newScheduledThreadPool(1);
     }
 }
