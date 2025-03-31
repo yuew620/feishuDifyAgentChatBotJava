e7c12853718fbbd94e2c5c1becb33cc614b3e350
Wang Yue <yyuewang@amazon.com>
2025-03-31 22:19:12 +0800

    test‘

--- src/main/java/com/sdxpub/feishubot/model/dify/DifyRequest.java
+++ src/main/java/com/sdxpub/feishubot/model/dify/DifyRequest.java
@@ -1,15 +1,6 @@
 package com.sdxpub.feishubot.model.dify;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class DifyRequest {
     private String query;
     private String user;
@@ -22,24 +13,79 @@ public class DifyRequest {
     
     private Inputs inputs;
 
-    @Data
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
+    public DifyRequest() {
+    }
+
+    public DifyRequest(String query, String user, String conversationId, String responseMode, Inputs inputs) {
+        this.query = query;
+        this.user = user;
+        this.conversationId = conversationId;
+        this.responseMode = responseMode;
+        this.inputs = inputs;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getConversationId() {
+        return conversationId;
+    }
+
+    public void setConversationId(String conversationId) {
+        this.conversationId = conversationId;
+    }
+
+    public String getResponseMode() {
+        return responseMode;
+    }
+
+    public void setResponseMode(String responseMode) {
+        this.responseMode = responseMode;
+    }
+
+    public Inputs getInputs() {
+        return inputs;
+    }
+
+    public void setInputs(Inputs inputs) {
+        this.inputs = inputs;
+    }
+
     public static class Inputs {
         private String history;
+
+        public Inputs() {
+        }
+
+        public Inputs(String history) {
+            this.history = history;
+        }
+
+        public String getHistory() {
+            return history;
+        }
+
+        public void setHistory(String history) {
+            this.history = history;
+        }
     }
 
     public static DifyRequest createRequest(String userId, String query, String conversationId) {
-        return DifyRequest.builder()
-                .query(query)
-                .user(userId)
-                .conversationId(conversationId)
-                .responseMode("streaming")
-                .inputs(Inputs.builder()
-                        .history("null")
-                        .build())
-                .build();
+        Inputs inputs = new Inputs("null");
+        return new DifyRequest(query, userId, conversationId, "streaming", inputs);
     }
 
     public static DifyRequest createInitialRequest(String userId, String query) {
